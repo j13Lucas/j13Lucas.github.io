d3c0f30c399800766f4cbb86461453f7fc727008
Jerry Lucas <141825992+j13Lucas@users.noreply.github.com>
2023-12-01 01:43:58 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://j13Lucas.github.io/',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Jerry Lucas',
+  role: 'Computer Vision Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  "Hello! I'm Jerry 👋 , a passionate Computer Vision enthusiast. With a solid foundation in computer science and a keen interest in the realms of machine learning and AI, I am currently deepening my knowledge and skills in the specialized field of Computer Vision. Although I'm early in my professional journey, I've always been intrigued by the capability of machines to 'see' and 'understand' their surroundings. Recognizing the transformative potential of this technology across various industries, from healthcare to autonomous vehicles, I am dedicated to utilize it’s power to create solutions that can make a real difference in people's lives",
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/j13lucas/',
+    github: 'https://github.com/j13Lucas',
   },
 }
 
@@ -21,51 +21,40 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Sunglasses Filter Using OpenCV',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Apply sunglasses filter on video/webcam as input',
+    stack: ['Python', 'OpenCV'],
+    sourceCode: 'https://github.com/j13Lucas/Sunglasses',
+    livePreview: 'https://example.com',
   },
   {
-    name: 'Project 2',
+    name: 'Object Detection And Tracking',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Detect Object and track it through video/webcam input',
+    stack: ['Python', 'OpenCV'],
+    sourceCode: 'https://github.com/j13Lucas/ObjectDetectionAndTracking',
+    livePreview: 'https://example.com',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Python',
+  'OpenCV',
+  'PyTorch',
+  'Keras'.
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'jerry.lucas@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
