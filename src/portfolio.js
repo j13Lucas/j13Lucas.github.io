1d14708e60197e7ed045bc3ae5b5974c444306f9
Jerry Lucas <141825992+j13Lucas@users.noreply.github.com>
2023-12-01 16:49:21 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://j13Lucas.github.io/',
-  title: 'JS.',
+  title: 'JL.',
 }
 
 const about = {
